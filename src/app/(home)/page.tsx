f0bb81ca9e00a8800d75874dec62ff434eb0c107
IBTSeokhoLee <162235845+IBTSeokhoLee@users.noreply.github.com>
2026-05-12 17:40:43 +0900

    Update page.tsx

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -6,6 +6,8 @@ import ContentBlocks from '@/components/intro/IntroContentBlock.client'
 import IntroComponent4 from '@/components/intro/IntroComponent4'
 
 const IntroPage = async () => {
+  
+  /*
   const res = await fetchPostData(0) //데이터 불러오는 딜레이가 아님
 
   return (
@@ -26,6 +28,21 @@ const IntroPage = async () => {
       <IntroComponent7 data={res.data} />
     </div>
   )
+  */
+      // public/index.html 연결
+  return (
+    <iframe
+      src="/index.html"
+      style={{
+        width: '100%',
+        height: '100vh',
+        border: 'none',
+      }}
+    />
+  )
+}
+
+export default IntroPage
 }
 
 export default IntroPage
